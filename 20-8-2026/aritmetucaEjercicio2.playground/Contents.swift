e885a84c36ecf5ef75f8d7c5564ca2a84d44a8ff
Angel Uziel Jimenez Marquez <angel.uziel360@gmail.com>
2026-08-20 14:37:20 -0600

    ejercicio 2

---
+++ 20-8-2026/aritmetucaEjercicio2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+//datos
+let  width = 10
+let  height = 20
+
+//operaciones
+let  area = width * height
+
+let roomArea = area/2
+
+let perimeter = width + width * height + height
+
+//resultados
+print(area)
+print(roomArea)
+print(perimeter)
+
