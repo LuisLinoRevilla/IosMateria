8884e1fe9e341685bd70a1ca8d43f623209e5581
LuisLinoRevilla <linorevilla@comunidad.unam.mx>
2026-09-24 14:28:31 -0600

    loops

--- 24-09-2026/ejercicio1.playground/Contents.swift
+++ 24-09-2026/ejercicio1.playground/Contents.swift
@@ -1,13 +1,31 @@
 import UIKit
 
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-for index in 1...100{
+print("--- Ejemplo 1 con FOR ---")
+for index in 1...100 {
     print(index)
 }
 
+print("\n--- Ejemplo 1 con WHILE ---")
+var index = 1
+while index <= 100 {
+    print(index)
+    index += 1
+}
+
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print("\n--- Ejemplo 2 con FOR ---")
 for (index, letter) in alphabet.enumerated() {
     print("\(index): \(letter)")
+}
+
+print("\n--- Ejemplo 2 con WHILE ---")
+let alphabetArray = Array(alphabet)
+var i = 0
+while i < alphabetArray.count {
+    let letter = alphabetArray[i]
+    print("\(i): \(letter)")
+    i += 1
 }
 
 let stateCapitals: [String: String] = [
@@ -16,6 +34,13 @@ let stateCapitals: [String: String] = [
     "New York": "Albany"
 ]
 
+print("\n--- Ejemplo 3 con FOR ---")
 for (state, capital) in stateCapitals {
+    print("La capital de \(state) es \(capital)")
+}
+
+print("\n--- Ejemplo 3 con WHILE ---")
+var iterator = stateCapitals.makeIterator()
+while let (state, capital) = iterator.next() {
     print("La capital de \(state) es \(capital)")
 }
